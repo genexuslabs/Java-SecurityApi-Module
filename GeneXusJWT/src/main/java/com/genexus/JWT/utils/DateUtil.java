27c712098755b95381f7b61e3de931b680e2dc21
sgrampone <sgrampone@genexus.com>
2020-06-30 17:16:06 -0300

    Fix JWT DateUtil methods

--- GeneXusJWT/src/main/java/com/genexus/JWT/utils/DateUtil.java
+++ GeneXusJWT/src/main/java/com/genexus/JWT/utils/DateUtil.java
@@ -17,15 +17,15 @@ public final class DateUtil extends DateUtilObject{
 	public String currentPlusSeconds(long seconds) {
 		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
 		LocalDateTime now = LocalDateTime.now();
-		now.plusSeconds(seconds);
-		return dtf.format(now);
+		LocalDateTime aux = now.plusSeconds(seconds);
+		return dtf.format(aux);
 	}
 	
 	public String currentMinusSeconds(long seconds) {
 		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
 		LocalDateTime now = LocalDateTime.now();
-		now.minusSeconds(seconds);
-		return dtf.format(now);
+		LocalDateTime aux = now.minusSeconds(seconds);
+		return dtf.format(aux);
 	}
 	/******** EXTERNAL OBJECT PUBLIC METHODS - END ********/
 
