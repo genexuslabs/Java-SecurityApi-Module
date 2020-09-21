6cd01eae67263dace5bf23600b61e38c27724369
sgrampone <30872877+sgrampone@users.noreply.github.com>
2020-09-21 09:54:25 -0300

    Adds FromBase64 & ToBase64 to PrivateKey (#34)

--- SecurityAPICommons/src/main/java/com/genexus/securityapicommons/commons/PrivateKey.java
+++ SecurityAPICommons/src/main/java/com/genexus/securityapicommons/commons/PrivateKey.java
@@ -7,4 +7,6 @@ public abstract class PrivateKey extends Key{
 	}
 	public abstract boolean load(String path);
 	public abstract boolean loadPKCS12(String path, String alias, String password);
+	public abstract boolean fromBase64(String base64);
+	public abstract String toBase64();
 }
